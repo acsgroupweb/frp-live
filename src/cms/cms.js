994d66544d9ec1498a53d10697463df3c5f8d664
Todd <127229502+acsgroupweb@users.noreply.github.com>
2023-05-05 02:50:22 -0500

    cms testing 2

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,38 @@
-// import CMS from 'netlify-cms-app'
+import CMS from "netlify-cms-app"
+import slugify from "slugify"
 
+const slugifyTeamName = async (values) => {
+  if (values.name) {
+    const slug = slugify(values.name, { lower: true, strict: true, locale: "en" }).replace(/[^a-zA-Z0-9-]/g, '')
+    values.slug = `team/${slug}`
+  }
+  return values
+}
 
-
-
-// import "../styles/global.css"
-
-
-
+CMS.init({
+  config: {
+    backend: {
+      name: "git-gateway",
+    },
+    media_folder: "static/uploads",
+    public_folder: "/uploads",
+    collections: [/* Your collections */],
+  },
+  // Add this part
+  options: {
+    load_config_file: false,
+  },
+  // Add this part
+  async onInit() {
+    CMS.registerEventListener({
+      name: "preSave",
+      handler: async ({ entry }) => {
+        if (entry.get("collection") === "team") {
+          const values = entry.get("data").toJS()
+          const newValues = await slugifyTeamName(values)
+          entry.get("data").merge(newValues)
+        }
+      },
+    })
+  },
+})
--- static/content/team/map-templatekey-team-name-todd-lambert-jobtitle-site-deverloper-credentials-ninja-order-5-profilepicture-static-images-default-user-png-body-todd-is-a-ninja-now-you-see-him-now-you-dont.md
+++
@@ -1,9 +0,0 @@
----
-layout: team
-name: Todd Lambert
-jobTitle: Site Deverloper
-credentials: Ninja
-order: 5
-profilePicture: /static/images/default-user.png
----
-Todd is a ninja - now you see him, now you dont.
